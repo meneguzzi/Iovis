ad09be1b1dfb8d85efb7e46fc19d7f5a21e4bd7e
Felipe Meneguzzi <meneguzzi@users.noreply.github.com>
2009-02-23 12:18:10 +0000

    First draft of puts method, before algorithm optimization.

---
+++ examples/cleaners/putsAgent.asl
@@ -0,0 +1,8 @@
+text("My text").
+
+startTest.
+
++startTest : text(A) & B=2
+	<- act.puts("The value of variable A is #{A}");
+	   act.puts("The value of variable B is #{B+2}").
+	   
--- src/act/puts.java
+++ src/act/puts.java
@@ -1,17 +1,18 @@
 package act;
 
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.InternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
+import jason.asSyntax.ASSyntax;
 import jason.asSyntax.StringTerm;
 import jason.asSyntax.StringTermImpl;
 import jason.asSyntax.Term;
-import jason.asSyntax.VarTerm;
+import jason.asSyntax.parser.ParseException;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * <p>
@@ -23,7 +24,9 @@ import jason.asSyntax.VarTerm;
  * string parameter, containing escaped variable names that are replaced by
  * their bindings in the current intention's unifier. Terms are made ground
  * according to the current unifying function before being printed out. No new
- * line is printed after the parameters.
+ * line is printed after the parameters. In this version a user can also 
+ * include any Jason expression (logical or arithmetic) that will be replaced
+ * by it's evaluated value.
  * 
  * <p>
  * The precise format and output device of the message is defined by the Java
@@ -54,6 +57,7 @@ import jason.asSyntax.VarTerm;
  * 
  */
 
+@SuppressWarnings("serial")
 public class puts extends DefaultInternalAction {
 
 	private static InternalAction singleton = null;
@@ -64,7 +68,8 @@ public class puts extends DefaultInternalAction {
 		return singleton;
 	}
 
-	Pattern regex = Pattern.compile("#\\{\\p{Upper}\\p{Alnum}*\\}");
+	//Pattern regex = Pattern.compile("#\\{\\p{Upper}\\p{Alnum}*\\}");
+	Pattern regex = Pattern.compile("#\\{\\p{ASCII}+\\}");
 
 	@Override
 	public Object execute(TransitionSystem ts, Unifier un, Term[] args)
@@ -91,7 +96,18 @@ public class puts extends DefaultInternalAction {
 				 */
 				String sVar = matcher.group();
 				sVar = sVar.substring(2, sVar.length() - 1);
-				alVariables.add(un.get(sVar));
+				try {
+					Term t = ASSyntax.parseTerm(sVar);
+					t.apply(un);
+					alVariables.add(t);
+				} catch (ParseException pe) {
+					// TODO: handle exception
+					// TODO: Decide whether or not we should ignore the exception and print the call instead
+					// Right now, if I get a parse error from ASSyntax, I just print the original escaped
+					// sequence, so a user can see that his/her expression was problematic
+					alVariables.add(new StringTermImpl("#{"+sVar+"}"));
+				}
+				
 			}
 
 			matcher.reset();
@@ -105,12 +121,7 @@ public class puts extends DefaultInternalAction {
 
 		if (args[args.length - 1].isVar()) {
 			StringTerm stRes = new StringTermImpl(sb.toString());
-			VarTerm var = (VarTerm) args[args.length - 1];
-			if (un.unifies(stRes, var)) {
-				return var.apply(un);
-			} else {
-				return false;
-			}
+			return un.unifies(stRes, args[args.length - 1]);
 		} else {
 			ts.getLogger().info(sb.toString());
 			return true;
